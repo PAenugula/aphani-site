66525475de777257295a62c4c2e9f0b130b584ae
Phaniraj Aenugula <phanirajaenugula@gmail.com>
2025-01-09 01:45:17 -0500

    fix build error

--- src/routes/ask_phani/+server.js
+++ src/routes/ask_phani/+server.js
@@ -2,11 +2,11 @@ import { json } from "@sveltejs/kit";
 import { env } from '$env/dynamic/private';
 import { GoogleGenerativeAI } from '@google/generative-ai';
 import promptFile from '$lib/about_phani/prompt.txt';
-import phani_resume from '$lib/about_phani/phani_resume.txt';
+import phaniResumeFile from '$lib/about_phani/phani_resume.txt';
 import { read } from '$app/server';
 
 const prompt = await read(promptFile).text();
-const phani_resume = await read(phani_resume).text();
+const phani_resume = await read(phaniResumeFile).text();
 
 export async function POST({ request, cookies }) {
     const { question } = await request.json();
